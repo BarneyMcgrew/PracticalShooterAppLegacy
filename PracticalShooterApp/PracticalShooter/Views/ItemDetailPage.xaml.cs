af435ffe892a94f0007092e1e614a56658eb8211
Aryan Alipour <aryan.alipour@outlook.com>
2020-12-17 03:48:19 +0000

    Update Solution Structure

---
+++ PracticalShooterApp/PracticalShooter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PracticalShooter.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PracticalShooter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
