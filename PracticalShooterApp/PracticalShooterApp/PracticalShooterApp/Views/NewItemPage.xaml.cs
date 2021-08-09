15c4ac05a574cac43c2b2ce27eb4aee765819256
Arius Alipour <aryan.alipour@outlook.com>
2021-08-09 23:54:19 +0100

    New Solution

---
+++ PracticalShooterApp/PracticalShooterApp/PracticalShooterApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PracticalShooterApp.Models;
+using PracticalShooterApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PracticalShooterApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
