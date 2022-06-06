2c5150a64342e1c5ba75198efdf038fc22bc6bf1
Arius Alipour <aryan.alipour@outlook.com>
2022-06-06 20:52:29 +0100

    Add new Versioning for beta release with file explorer

--- PracticalShooterApp/PracticalShooterApp/PracticalShooterApp/ViewModels/FileExplorerPageViewModel.cs
+++ PracticalShooterApp/PracticalShooterApp/PracticalShooterApp/ViewModels/FileExplorerPageViewModel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89d0e432675a4cf89ad48ca95ef2e18cbe0631b7c5b835a077d241cc1ce0d9df
-size 2671
+oid sha256:4a59d9b25637cb30f0507e41be719b316dc19baaeed394e0bbcdda8ff22471e0
+size 8705
