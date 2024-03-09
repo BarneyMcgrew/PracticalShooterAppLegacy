2f65c660baa876335729b7d599782ade48daa36c
Aryan Alipour <aryan.alipour@outlook.com>
2024-03-09 23:56:59 +0000

    Add introduction of users

---
+++ CMSPracticalShooterApp/fire-cms/src/collections/userCollection.js
@@ -0,0 +1,34 @@
+import {
+    buildCollection
+} from "Firecms";
+
+export const userCollection = buildCollection({
+    name: "Users",
+    singularName: "User",
+    group: "Account Management",
+    path: "users",
+    permissions: ({}) => ({
+        edit: true,
+        create: false,
+        delete: true
+    }),
+    properties: {
+        userId: {
+            name: "User Id",
+            validation: { required: true },
+            dataType: "string"
+        },
+        email: {
+            name: "Email Address",
+            validation: { required: false },
+            dataType: "string"
+        },
+        displayName: {
+            name: "Display Name",
+            validation: { required: true },
+            dataType: "string"
+        },
+
+    }
+})
+
